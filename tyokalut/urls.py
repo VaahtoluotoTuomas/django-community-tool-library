dfd7e30d63d2dfaf468f9ec9bf21c41f272fbc5f
VaahtoluotoTuomas <tuomas.vaahtoluoto@gmail.com>
2026-04-05 18:50:48 +0300

    Add: details page for spesific tools + Tailwind styles

--- tyokalut/urls.py
+++ tyokalut/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tyokalu_lista, name='työkalu_lista'),
+    path('', views.tyokalu_lista, name='tyokalu_lista'),
+    path('tyokalu/<int:tyokalu_id>/', views.tyokalu_tiedot, name='tyokalu_tiedot'),
 ]
--- tyokalut/views.py
+++ tyokalut/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Tool
 
 def tyokalu_lista(request):
     tyokalut = Tool.objects.all().prefetch_related('manufacturers', 'tags')
     return render(request, 'tyokalut/lista.html', {'tyokalut': tyokalut})
+
+def tyokalu_tiedot(request, tyokalu_id):
+    tyokalu = get_object_or_404(Tool, id=tyokalu_id)
+    return render(request, 'tyokalut/tiedot.html', {'tyokalu': tyokalu})
