609d6f23c7c50cf98b09a70a102040a791f3ef5c
VaahtoluotoTuomas <tuomas.vaahtoluoto@gmail.com>
2026-04-05 20:42:48 +0300

    Add: register and login pages using Django's built-in login system. They work now and it's possible to create new users , but loaning and logging out is not yet possible.

--- tyokalut/urls.py
+++ tyokalut/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.tyokalu_lista, name='tyokalu_lista'),
     path('tyokalu/<int:tyokalu_id>/', views.tyokalu_tiedot, name='tyokalu_tiedot'),
+    path('kirjaudu/', auth_views.LoginView.as_view(template_name='tyokalut/kirjaudu.html'), name='kirjaudu'),
+    path('kirjaudu-ulos/', auth_views.LogoutView.as_view(), name='kirjaudu_ulos'),
+    path('rekisteroidy/', views.rekisteroidy, name='rekisteroidy'),
 ]
--- tyokalut/views.py
+++ tyokalut/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Tool
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 def tyokalu_lista(request):
     tyokalut = Tool.objects.all().prefetch_related('manufacturers', 'tags')
@@ -8,3 +11,15 @@ def tyokalu_lista(request):
 def tyokalu_tiedot(request, tyokalu_id):
     tyokalu = get_object_or_404(Tool, id=tyokalu_id)
     return render(request, 'tyokalut/tiedot.html', {'tyokalu': tyokalu})
+
+def rekisteroidy(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('tyokalu_lista')
+    else:
+            form = UserCreationForm()
+        
+    return render(request, 'tyokalut/rekisteroidy.html', {'form': form})
